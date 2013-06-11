54be701b9957a3609e6fbd62c63d69164e47768b
Patres <p.fuhrmann@myafricanidea.com>
2013-06-11 23:42:42 +0100

    New feature for handling favourites - no fancy design

--- photos.js
+++ photos.js
@@ -1,6 +1,28 @@
 /*global jQuery*/
 
 var setupPhotos = (function ($) {
+	function getCookie(c_name) {
+		var c_value = document.cookie;
+		var c_start = c_value.indexOf(" " + c_name + "=");
+		
+		if (c_start == -1) {
+			c_start = c_value.indexOf(c_name + "=");
+		}
+		
+		if (c_start == -1) {
+			c_value = null;
+		} else {
+			c_start = c_value.indexOf("=", c_start) + 1;
+			var c_end = c_value.indexOf(";", c_start);
+			
+			if (c_end == -1) {
+				c_end = c_value.length;
+			}
+			c_value = unescape(c_value.substring(c_start,c_end));
+		}
+		return c_value;
+	}
+	
     function each (items, callback) {
         var i;
         for (i = 0; i < items.length; i += 1) {
@@ -60,12 +82,33 @@ var setupPhotos = (function ($) {
         img.src = photo;
         return img;
     }
+    
+    var favArr = (getCookie('fav')) ? getCookie('fav').split("|") : [];
+    function handleClick(e) {
+    	var elm = e.target;
+    	var imgSrc = elm.parentNode.getElementsByTagName('img')[0].src;
+    	
+    	if (elm.className === 'icon-heart-empty') {
+            elm.className = 'icon-heart';
+            favArr.push(imgSrc);
+    	} else {
+        	var index = favArr.indexOf(imgSrc);
+        	elm.className = 'icon-heart-empty';
+        	favArr.splice(index, 1);
+    	}
+    	document.cookie = 'fav=' + favArr.join('|');
+    }
 
     function imageAppender (id) {
         var holder = document.getElementById(id);
+
         return function (img) {
             var elm = document.createElement('div');
             elm.className = 'photo';
+            var likeElm = document.createElement('i');
+            likeElm.className = (favArr.indexOf(img.src) >= 0) ? 'icon-heart' : 'icon-heart-empty';
+            likeElm.addEventListener("click", handleClick, false);
+            elm.appendChild(likeElm);
             elm.appendChild(img);
             holder.appendChild(elm);
         };
